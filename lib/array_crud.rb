6d04e71e082bfdf9a52f9ac1734c3829ad90378f
Zaire  Swailes <swaileszaire@gmail.com>
2020-01-21 04:24:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,33 +1,33 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+["Pumpkin", "FireFly", "Full Moon"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+"Costume" = create_an_array.push
 end
 
 def add_element_to_start_of_array(array, element)
-  
+"Spirits" = create_an_array.unshift
 end
 
 def remove_element_from_end_of_array(array)
-  
+create_an_array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+create_an_array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+create_an_array[0]
 end
 
 def retrieve_first_element_from_array(array)
-  
+
 end
 
 def retrieve_last_element_from_array(array)
